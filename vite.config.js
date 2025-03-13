cba16e95830caefc17f0b48f4c4d065a3ac345a0
William Gay <williamgay360@gmail.com>
2025-03-13 12:40:57 -0400

    trying url as config base

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/home/',
+  base: 'https://gt2ai.github.io/home/',
   plugins: [
     react(),
     VitePWA({
