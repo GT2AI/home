7210bad4c5808ecdb353d09958a2bc586b157d69
William Gay <williamgay360@gmail.com>
2025-03-13 12:39:06 -0400

    changing the vite config

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ColorModeScript initialColorMode={theme.config.initialColorMode} />
     <ChakraProvider theme={theme}>
-      <Router>
+      <Router basename="/home">
         <App />
       </Router>
     </ChakraProvider>
